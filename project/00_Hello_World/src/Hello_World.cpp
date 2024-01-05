73cb7d415fc19a683038de957e8363528fd738ca
liubo00 <liubo@zerozero.cn>
2024-01-05 14:27:17 +0800

    khklklk

---
+++ project/00_Hello_World/src/Hello_World.cpp
@@ -0,0 +1,36 @@
+// *********************************************************
+// file name：Hello_World.cpp
+// author：liubo
+// date：2024.01.05
+// describe：template
+// *********************************************************
+#include<iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(){
+    cout<<"Hello World"<<endl;
+    return 0;
+}
+
+
+
+
+
+
+
+//! 
+//! 
+//! Create a subscription with the specified unsubscribe functor.
+//!
+//! \param[in] unsubscribe Calling this functor will unsubscribe the
+//!                        associated observer.
+//! \note This is for internal use by subject instances.
+
+
+
+
+
+
