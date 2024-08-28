6d9f4c193eeeed1c3986b3c0b49ccaaf98937e8c
liubo00 <liubo@zerozero.cn>
2024-08-28 10:09:30 +0800

    result

--- code/Cpp_primer_plus/29_class_dyamic_memery/src/string_bad/string_bad.cpp
+++ code/Cpp_primer_plus/29_class_dyamic_memery/src/string_bad/string_bad.cpp
@@ -20,29 +20,25 @@ using std::endl;
 int StringBad::num_strings = 0;
 
 // class methods
-
 // construct StringBad from C string
-
 StringBad::StringBad(const char* s)
 {
-len = std::strlen(s); // set size
+    len = std::strlen(s);  // set size
 
-str = new char[len + 1]; //allot storage
-std::strcpy(str,s); //initialize pointer
+    str = new char[len + 1];  // allot storage
+    std::strcpy(str, s);      // initialize pointer
 
-num_strings++; //set object count
-cout << num_strings << ": \" "<< str << "\" object created\n"; // For Your Information
+    num_strings++;                                                   // set object count
+    cout << num_strings << ": \" " << str << "\" object created\n";  // For Your Information
 }
 
 StringBad::StringBad()  // default constructor
-
 {
-len = 4;
-str - new char[4];
-std::strcpy(str, "C++"); // default string
-num_strings++;
-cout << num_strings << ": \"" << str << "\" default object created\n"; //FYI
-
+    len = 4;
+    str = new char[4];
+    std::strcpy(str, "C++");  // default string
+    num_strings++;
+    cout << num_strings << ": \"" << str << "\" default object created\n";  // FYI
 }
 
 StringBad::~StringBad()
