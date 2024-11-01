3b50bb8d2272222fc54cb566571b07f65246e3ba
liubo <liubo@zerozero.cn>
2024-11-01 10:43:14 +0800

    dgfg

--- 04_CPP11/chrono/code/clock/src/main.cpp
+++ 04_CPP11/chrono/code/clock/src/main.cpp
@@ -49,6 +49,7 @@ int main()
     // 将获取的时间转换成time_t类型
     time_t tm2 = system_clock::to_time_t(epoch);
     cout << tm2 << endl;
+    printf("------%d\n", tm2);
     // ctime()函数将time_t类型的时间转化成字符串格式,这个字符串自带换行符
     cout << "新纪元时间:      " << ctime(&tm2);
 
