63f6c78bd4e8bfb168b4578a97dac66db000ba52
liubo00 <liubo@zerozero.cn>
2024-11-18 19:42:26 +0800

    lkdjhskgjksdfljkl

--- 02_data_structure/code/BlockingQueue/src/main.cpp
+++ 02_data_structure/code/BlockingQueue/src/main.cpp
@@ -9,12 +9,43 @@
 #include <iostream>  // 包含输入和输出操作
 #include <stdio.h>   // C语言的标准库，包含C语言流操作 printf等
 #include <pthread.h>
+#include <cstdio>
+#include <string>
+#include <unistd.h>
 
 using std::cin;
 using std::cout;
 using std::endl;
 
-pthread_cond_t cond;  // 全局 pthread_cond_t 变量
+int ticket = 1000;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;  // 全局 pthread_cond_t 变量
+
+
+
+
+
+using namespace std;
+
+void * GetTicket(void * args)
+{
+    string name = static_cast<const char *>(args);
+    while (true) {
+        pthread_mutex_lock(&lock);
+        // 线程进来都先排队
+        pthread_cond_wait(&cond, &lock);  // 为什么要有lock,后面就说
+        if (ticket > 0) {
+            usleep(1000);
+            cout << name << " 正在进行抢票: " << ticket << endl;
+            ticket--;
+            pthread_mutex_unlock(&lock);
+        } else {
+            pthread_mutex_unlock(&lock);
+            break;
+        }
+    }
+}
+
 /*****************************************************************************
  * | @fn     : XXXX
  * | @param  : - XXX XXX
@@ -29,20 +60,23 @@ int main()
 {
     printf("----------------begain------------------\n");
 
-    int rc;
-
-    // 显式初始化全局 pthread_cond_t 变量
-    rc = pthread_cond_init(&cond, NULL);
-    if (rc != 0) {
-        printf("Cond init failed: %d\n", rc);
-        return 1;
+    pthread_t t[5];
+    for (int i = 0; i < 5; ++i) {
+        char *name = new char[64];
+        snprintf(name, 64, "thread %d", i + 1);
+        pthread_create(t + i, nullptr, GetTicket, name);
     }
 
-    // ... 其他代码，包括线程创建和同步 ...
+    while (true) {
+        sleep(1);
+        // 主线程一秒唤醒一个线程
+        pthread_cond_signal(&cond);
+        std::cout << "main thread wakeup one thread..." << std::endl;
+    }
 
-    // 在不再需要条件变量时销毁它
-    //...
-
+    for (int i = 0; i < 5; i++) {
+        pthread_join(t[i], nullptr);
+    }
     printf("-----------------end-------------------\n");
     // getchar();
     return EXIT_SUCCESS;
--- 03_concurrency_program/code/02_mutex/src/main.cpp
+++ 03_concurrency_program/code/02_mutex/src/main.cpp
@@ -10,7 +10,9 @@
 #include <stdio.h>   // C语言的标准库，包含C语言流操作 printf等
 #include <string.h>  // C语言的标准库，包含字符串处理操作 strcpy等
 
-#include "XXX.hpp"
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
 
 using std::cin;
 using std::cout;
@@ -38,3 +40,50 @@ int main()
 /*****************************************************************************
  * end of file
  ******************************************************************************/
+
+
+
+
+
+int shared_data = 0;
+pthread_mutex_t mutex;
+
+void *threadFoo(void *arg)
+{
+    pthread_mutex_lock(&mutex);  // 加锁
+    while (1) {
+        shared_data++;
+        printf("threadFoo:shared_data=%d\n", shared_data);
+        if (shared_data == 10) {
+            pthread_mutex_unlock(&mutex);  // 执行完之后解锁
+            pthread_exit(NULL);
+        }
+        sleep(1);
+    }
+}
+void *threadBar(void *arg)
+{
+    pthread_mutex_lock(&mutex);  // 加锁
+    while (1) {
+        shared_data++;
+        printf("___threadBar:shared_data=%d\n", shared_data);
+        if (shared_data == 20) {
+            pthread_mutex_unlock(&mutex);  // 执行完之后解锁
+            pthread_exit(NULL);
+        }
+        sleep(1);
+    }
+}
+
+int main()
+{
+    pthread_t threadID1;
+    pthread_t threadID2;
+    pthread_mutex_init(&mutex, NULL);
+    pthread_create(&threadID1, NULL, (void *)threadFoo, NULL);
+    pthread_create(&threadID2, NULL, (void *)threadBar, NULL);
+    pthread_join(threadID1, NULL);
+    pthread_join(threadID2, NULL);
+    pthread_mutex_destroy(&mutex);
+    return 0;
+}
