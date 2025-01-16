dcde90c26a22e31010787257eec3c6092a297514
liubo00 <liubo@zerozero.cn>
2025-01-16 15:19:14 +0800

    adfkksafjk

--- 01_basic_example/02_space_input/src/space_input.cpp
+++ 01_basic_example/02_space_input/src/space_input.cpp
@@ -1,11 +1,12 @@
 /*****************************************************************************
- * | @file    : main.c
+ * | @file    : space_input.c
  * | @author  : liubo
  * | @version : V1.0.0
- * | @date    : 2024-09-01
+ * | @date    : 2024-01-28
  * --------------
- * | @brief   : main
+ * | @brief   : iosream deal with space
  ******************************************************************************/
+
 #include <stdio.h>   // C语言的标准库，包含C语言流操作 printf等
 #include <string.h>  // C语言的标准库，包含字符串处理操作 strcpy等
 #include <unistd.h>  // pause()头文件
@@ -17,21 +18,19 @@
 using std::cin;
 using std::cout;
 using std::endl;
-/*****************************************************************************
- * | @fn     : XXXX
- * | @param  : - XXX XXX
- * |           - XXX XXX
- * |           - XXX XXX
- * | @return :
- * --------------
- * | @brief  : XXX
- * |           XXX
- ******************************************************************************/
+
 int main()
 {
     printf("--------------------begain-------------------\n");
-    /**
-     * write your code here */
+    cout << "\n----------------空格输入 测试------------------" << endl;
+    float a;
+    int i1, i2;
+    char ch1, ch2;
+    cin >> i1 >> a >> i2 >> ch1 >> ch2;
+
+    cout << "i1:" << i1 << "  a:" << a << "  i2:" << i2 << "  ch1:" << ch1 << "  ch2:" << ch2 << endl;
+    // 输入：34  5.678  1a  b<CR>
+    // 输出：i1:34  a:5.678  i2:1  ch1:a  ch2:b
 
     printf("--------------------end----------------------\n");
     // getchar();
